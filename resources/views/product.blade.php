ad631fba3f012f2088babba9aba931ab0cbfd442
nqnam43 <nqnam43@gmail.com>
2024-11-09 10:45:51 +0700

    add code

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <section class="products mw-1220 m-auto">
         <div class="list-product-header ">
-            <a href="{{ route('home') }}" class="back-link">
+            <a href="{{ route('home-page') }}" class="back-link">
                 <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8.75 3.5L5.25 7L8.75 10.5" stroke="#767676" stroke-width="1.5" stroke-linecap="round"
                         stroke-linejoin="round" />
