8db1166294ed6b0068c47ca716d98a8c84df993b
nqnam43 <nqnam43@gmail.com>
2024-11-13 09:47:08 +0700

    change view

--- resources/views/read-book.blade.php
+++ resources/views/read-book.blade.php
@@ -1,9 +1,2 @@
-@extends('layouts.app-2')
-@section('title', 'Chi tiết sản phẩm')
-@section('content')
-    <iframe allowfullscreen="" style="width: 100%; height: 500px;"
-        src="https://heyzine.com/api1?pdf={{ route('products.get-book', ['code' => $code]) }}&k={{ config('heyzine.client_key') }}"></iframe>
-@endsection
-
-@push('scripts')
-@endpush
+<iframe allowfullscreen="" style="width: 100vw; height: 100vh;position: fixed;top: 0px;left: 0px;background: #0D0C14;"
+    src="https://heyzine.com/api1?pdf={{ route('products.get-book', ['code' => $code]) }}&k={{ config('heyzine.client_key') }}"></iframe>
