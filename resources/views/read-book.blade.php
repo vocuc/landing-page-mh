7827275098a6b008fcc3e16d6040db1d6a4b6d36
nqnam43 <nqnam43@gmail.com>
2024-11-13 14:27:11 +0700

    add code

--- resources/views/read-book.blade.php
+++ resources/views/read-book.blade.php
@@ -1,2 +1,2 @@
 <iframe allowfullscreen="" style="width: 100vw; height: 100vh;position: fixed;top: 0px;left: 0px;background: #0D0C14;"
-    src="https://heyzine.com/api1?pdf={{ route('products.get-book', ['code' => $code, 'v' => '2024']) }}&k={{ config('heyzine.client_key') }}&d=0&pn=1&fs=1"></iframe>
+    src="https://heyzine.com/api1?pdf={{ route('products.get-book', ['code' => $code, 'v' => '2024']) }}&k={{ config('heyzine.client_key') }}&d=0&pn=1&fs=1&prev_next=1"></iframe>
