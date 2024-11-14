ca4733cf74cc22f5bfee603df2c95bfcea7e89c8
nqnam43 <nqnam43@gmail.com>
2024-11-14 15:33:43 +0700

    add code

--- resources/views/read-book.blade.php
+++ resources/views/read-book.blade.php
@@ -1,2 +1,32 @@
-<iframe src="data:text/html;base64,{{ $base64Content }}"
-    height="100%" width="100%"></iframe>
+<script>
+    document.addEventListener('keydown', function(event) {
+        
+        if (event.ctrlKey && event.key.toLowerCase() === 'u') {
+            event.preventDefault();
+        }
+        if (event.ctrlKey && event.shiftKey && (event.key.toLowerCase() === 'j' || event.key.toLowerCase() === 'i')) {
+            event.preventDefault();
+        }
+        if (event.key === 'F12') {
+            event.preventDefault();
+        }
+    });
+
+    document.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+    });
+
+    function detectDevTools() {
+        const before = new Date().getTime();
+        debugger; // DevTools làm chậm thời gian thực thi ở đây
+        const after = new Date().getTime();
+
+        if (after - before > 100) { // Kiểm tra nếu thời gian trễ lớn hơn 100ms
+            alert("DevTools đang mở. Hãy đóng nó lại!");
+            window.location.href = "https://example.com"; // Chuyển hướng hoặc làm gì đó
+        }
+    }
+
+    setInterval(detectDevTools, 1000);
+</script>
+<iframe src="data:text/html;base64,{{ $base64Content }}" height="100%" width="100%"></iframe>
