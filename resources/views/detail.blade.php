4e38ac4fbfc864435e22df29e54734acbe7ab2e2
QuanBT <buithequantk40l@gmail.com>
2024-11-17 12:02:55 +0700

    no message

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -387,4 +387,5 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js">
     </script>
     <script src="{{ asset('js/detail.js') }}?v={{ filemtime(public_path('js/detail.js')) }}"></script>
+    <script src="{{ asset('js/index.js') }}"></script>
 @endpush
