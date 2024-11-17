beac54a5194e374aed2e5496e5bbc5b6323e4f29
QuanBT <buithequantk40l@gmail.com>
2024-11-17 11:56:34 +0700

    no message

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -125,6 +125,42 @@
         </div>
         <!--<div class="btn-custom btn-type-2">Liên hệ tư vấn</div>-->
     </div>
+
+    <section class="contact">
+        <div class="title">
+            <div class="title__group--sologan mw-1220 m-auto">
+                <h2 class="title__group--sologan-content">
+                    Hỗ Trợ
+                </h2>
+                <div class="title__group--sologan-sub-content">
+                    Hãy để lại thông tin để ThienNhai hỗ trợ bạn sớm nhé!
+                </div>
+                <div class="line-title"></div>
+            </div>
+        </div>
+        <div class="contact-form">
+            <form action="{{ route('contact-form.store') }}" class="form m-auto" id="contactForm">
+                @csrf
+                <div class="d-flex gap-12px ">
+                    <div class="flex-grow-1 d-flex flex-column f-w-12px">
+                        <input type="text" class=" input-custom" name="name" placeholder="Tên của bạn">
+                    </div>
+                    <div class="flex-grow-1 d-flex flex-column f-w-12px">
+                        <input type="text" class=" input-custom" name="email" placeholder="Địa chỉ email của bạn">
+
+                    </div>
+                </div>
+                <div>
+                    <textarea name="note" class="input-custom col-12" placeholder="Lời nhắn của bạn"></textarea>
+                </div>
+                <div>
+                    <button class="btn-custom btn-type-3">
+                        Gửi ngay
+                    </button>
+                </div>
+            </form>
+        </div>
+    </section>
     <!-- Modal -->
     <div class="modal fade" id="modalContactPay1" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
         aria-labelledby="modalContactPayLabel" aria-hidden="true">
