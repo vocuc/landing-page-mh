d9f418c5bbfcee4e47d46c1483b3908ccee4a210
QuanBT <buithequantk40l@gmail.com>
2025-05-12 09:43:41 +0700

    update QR

--- config/constants.php
+++ config/constants.php
@@ -3,8 +3,8 @@ return [
     'DEFAULT_PAGE_SIZE' => 15,
     'QR_INFORMATION' => [
         'URL_GENERATE_QRCODE' => 'https://qr.limcorp.vn/qrcode.png?',
-        'BANK_NUMBER' => '970422',
-        'ACCOUNT_NUMBER' => '1200104838688'
+        'BANK_NUMBER' => '970423',
+        'ACCOUNT_NUMBER' => '71702091999'
     ],
     'MAIL_CONTACT' => 'ttn@gmail.com'
 ];
